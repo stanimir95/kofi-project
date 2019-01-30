6bd3bf8cdb2873c8d86649f0b78be5d49319c429
stanimir95 <43109511+stanimir95@users.noreply.github.com>
2019-01-30 12:59:43 +0200

    Update binsmain.go

--- binsmain.go
+++ binsmain.go
@@ -10,14 +10,15 @@ import (
 	"syscall"
 )
 
-// bins run <container> command arguments
-// go run main.go run command arguments
+// kofi run command arguments
+// go run binsmain.go run command arguments
 func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
 	case "child":
 		child()
+// 
 	case "init":
 		dlandunpack()
 	default:
@@ -44,7 +45,7 @@ func run() {
 }
 
 func child() {
-	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
+	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid()) // Expected output: [<for instance bin/bash> started as PID 1]
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -65,16 +66,18 @@ func must(err error) {
 	}
 }
 
+//from here (line 68 to the end) is the "init" command)
+// dlunpack creates a folder and downloads an archive in the directory that "kofi init" has been run
 func dlandunpack() {
 	os.Mkdir(filepath.Join("ubuntufs"), 0777)
 	os.Chdir("./ubuntufs")
 	fileUrl := "https://partner-images.canonical.com/core/trusty/current/ubuntu-trusty-core-cloudimg-amd64-root.tar.gz"
-
+	
 	err := DownloadFile("ubuntu.tar.gz", fileUrl)
 	if err != nil {
 		panic(err)
 	}
-
+// Extracts the archive in the newly created folder 
 	cmd := exec.Command("tar", "-xvzf", "ubuntu.tar.gz")
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -85,14 +88,14 @@ func dlandunpack() {
 
 func DownloadFile(filepath string, url string) error {
 
-	// Create the file
+	// Create the archive file
 	out, err := os.Create(filepath)
 	if err != nil {
 		return err
 	}
 	defer out.Close()
 
-	// Get the data
+	// Download 
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
